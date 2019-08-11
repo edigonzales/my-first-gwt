a50f528e027c3db2dd521c9a89639acc28e02475
Stefan Ziegler <edi.gonzales@gmail.com>
2019-08-11 16:41:03 +0200

    works

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ mvn archetype:generate \
     -DarchetypeArtifactId=maven-archetype-quickstart \
     -DarchetypeVersion=1.3 -DinteractiveMode=false \
     -DgroupId=me -DartifactId=my-first-gwt -Dversion=HEAD-SNAPSHOT
+```
 
 ```
+mvn gwt:devmode
+```
+
 
 
---
+++ src/main/java/me/client/App.java
@@ -0,0 +1,16 @@
+package me.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Text;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class App implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        Text textNode = Document.get().createTextNode("Hello Stefan!");
+        RootPanel.getBodyElement().appendChild(textNode);
+    }
+
+}
